18c0fd1a759ca7c2595c5f601e0a632c1b27de46
albaderon <kravchukodessa@gmail.com>
2017-04-22 17:27:22 +0300

    Task 14,15,16,17

---
+++ kravchuk_hw4.py
@@ -0,0 +1,112 @@
+import math
+
+def print_delim(num_repits=80):
+    print('='*num_repits)
+    pass
+
+def pretty_print(value):
+    print('Result =', value)
+    print_delim()
+    pass
+
+def pretty_task(value):
+    print_delim()
+    print("Task number", value)
+    pass
+
+
+# ============================================================================================
+# Task 14. Написать функцию, которая будет проверять четность некоторого числа.
+# ============================================================================================
+
+pretty_task(14)
+
+def is_number_even(number):
+    return not number % 2
+
+    # result = number % 2
+    # if result == 0:
+    #     return True
+    # else:
+    #     return False
+
+num_val = int(input('Enter number... '))
+pretty_print(is_number_even(num_val))
+
+# ============================================================================================
+# Task 15. Написать функцию, которая отвечает на вопрос, пересекаются ли две заданные
+# окружности на плоскости. Каждая окружность задается координатами центра и радиусом.
+# ============================================================================================
+
+pretty_task(15)
+
+circle_x1 = 5
+circle_y1 = 5
+circle_r1 = 2
+
+circle_x2 = 10
+circle_y2 = 10
+circle_r2 = 7
+
+def is_circles_cross(x1,y1,r1,x2,y2,r2):
+    a = math.fabs(x1 - x2)
+    b = math.fabs(y1 - y2)
+    c = math.sqrt(a**2 + b**2)
+    return not c > (r1+r2)
+
+pretty_print(is_circles_cross(circle_x1,circle_y2,circle_r1,circle_x2,circle_y2,circle_r2))
+
+# ============================================================================================
+# Task 16. Два поезда движутся на скорости V1 и V2 навстречу друг другу.
+# Между ними 10 км. пути. Через 4 км пути первый поезд может свернуть на запасной путь.
+# При заданных скоростях узнать столкнутся ли поезда.
+
+# ============================================================================================
+
+pretty_task(16)
+
+train_v1 = 50
+train_v2 = 70
+
+def is_damage(v1,v2):
+    if 4/v1 > (10-4)/v2:
+        return True
+    else:
+        return False
+    # return not 4/v1 < (10-4)/v2
+
+pretty_print(is_damage(train_v1,train_v2))
+
+# ============================================================================================
+# Task 17. Написать функцию решения квадратного уравнения.
+# ============================================================================================
+
+pretty_task(17)
+
+def quadratic_equation(a,b,c):
+        d=b**2-4*a*c
+        if  d > 0:
+            x1=(-b+math.sqrt(d))/2*a
+            x2=(-b-math.sqrt(d))/2*a
+            return x1,x2
+            print('x1=%.1f x2=%.1f' % (x1,x2))
+        elif d == 0:
+            x1=-b/2*a
+            return x1
+            print('x1=x2=%.1f' % x1)
+        else:
+            print('D<0 results = NONE')
+
+a = int(input('Enter a... '))
+b = int(input('Enter b... '))
+c = int(input('Enter c... '))
+
+pretty_print(quadratic_equation(a,b,c))
+
+
+
+
+
+
+
+
