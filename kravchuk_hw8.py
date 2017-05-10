41da4076377126a873d542ff52d6fdc7af28fc09
albaderon <kravchukodessa@gmail.com>
2017-05-10 21:52:50 +0300

    add string.ascii

--- kravchuk_hw8.py
+++ kravchuk_hw8.py
@@ -1,5 +1,6 @@
 import math
 import random
+import string
 
 def print_delim(num_repits=80):
     print('='*num_repits)
@@ -26,12 +27,12 @@ def pretty_task(value):
 pretty_task(31)
 
 def password():
-    symbol = [chr(x) for x in range(97,123)]
-    symbol_up = [chr(c) for c in range(65,91)]
-
+    symbol = string.ascii_lowercase
+    symbol_up = string.ascii_uppercase
+    digits = string.digits
     password = []
 
-    password.append(str(random.randint(0,9)))                           #required condition
+    password.append(str(random.choice(digits)))                           #required condition
     password.append(random.choice(symbol))                              #required condition
     password.append(random.choice(symbol_up))                           #required condition
     password.append('_')                                                #required condition
@@ -42,7 +43,7 @@ def password():
         elif r == 1:
             password.append(random.choice(symbol_up))
         else:
-            password.append(str(random.randint(0,9)))
+            password.append(str(random.choice(digits)))
     random.shuffle(password)
     password_string = ''.join(password)
     return password_string
