41639368ceb5a6f59638308c5e428d789394f0d0
albaderon <kravchukodessa@gmail.com>
2017-05-10 19:34:34 +0300

    random.choice of list

--- kravchuk_hw8.py
+++ kravchuk_hw8.py
@@ -28,20 +28,21 @@ pretty_task(31)
 def password():
     symbol = [chr(x) for x in range(97,123)]
     symbol_up = [chr(c) for c in range(65,91)]
+
     password = []
 
     password.append(str(random.randint(0,9)))                           #required condition
-    password.append(symbol[random.randint(0,(len(symbol)-1))])          #required condition
-    password.append(symbol_up[random.randint(0,(len(symbol_up)-1))])    #required condition
+    password.append(random.choice(symbol))                              #required condition
+    password.append(random.choice(symbol_up))                           #required condition
     password.append('_')                                                #required condition
     for i, elem in enumerate(range(0,4)):
         r = random.randint(0,2)
         if r == 0:
-            password.append(symbol[random.randint(0,(len(symbol)-1))])
+            password.append(random.choice(symbol))
         elif r == 1:
-            password.append(symbol_up[random.randint(0,(len(symbol_up)-1))])
+            password.append(random.choice(symbol_up))
         else:
-            password.append(str(random.randint(0,10)))
+            password.append(str(random.randint(0,9)))
     random.shuffle(password)
     password_string = ''.join(password)
     return password_string
