7a15ce79596ce9eaa068e7375b33f35004c5f39a
albaderon <kravchukodessa@gmail.com>
2017-05-12 15:33:59 +0300

    Test 1, Task 5

---
+++ Test_1_5.py
@@ -0,0 +1,40 @@
+import math
+import random
+
+def print_delim(num_repits=80):
+    print('='*num_repits)
+    pass
+
+def pretty_print(value):
+    print('Result =', value)
+    print_delim()
+    pass
+
+def pretty_task(value):
+    print_delim()
+    print("Task number", value)
+    pass
+
+# ============================================================================================
+# Task 5.Создать программу, выводящую на экран ближайшее к 10 из двух чисел,
+# введенных пользователем. Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.
+# ============================================================================================
+
+pretty_task(5)
+
+def is_short_number(x,y):
+    compare_number = 10
+
+    if abs(10-x) < abs(10-y):
+        result = x
+    elif  abs(10-x) == abs(10-y):
+        result = x,y
+    else:
+        result = y
+    return result
+
+x = int(input('Type first number'))
+y = int(input('Type second number'))
+
+pretty_print(is_short_number(x,y))
+
