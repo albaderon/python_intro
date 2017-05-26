63e0c6ae51bfdf938a74000319d89f9e62ae485f
albaderon <kravchukodessa@gmail.com>
2017-05-26 09:33:08 +0300

    add pretty modul

---
+++ pretty.py
@@ -0,0 +1,13 @@
+def print_delim(num_repits=80):
+    print('='*num_repits)
+    pass
+
+def pretty_print(value):
+    print('Result =', value)
+    print_delim()
+    pass
+
+def pretty_task(value):
+    print_delim()
+    print("Task number", value)
+    pass
