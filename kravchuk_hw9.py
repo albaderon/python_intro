1d0e6ee1625116089ce19325531ee94cc1a23994
albaderon <kravchukodessa@gmail.com>
2017-05-26 09:33:59 +0300

    Task 32

--- kravchuk_hw9.py
+++ kravchuk_hw9.py
@@ -2,27 +2,14 @@ import math
 import random
 import string
 import pickle
-
-def print_delim(num_repits=80):
-    print('='*num_repits)
-    pass
-
-def pretty_print(value):
-    print('Result =', value)
-    print_delim()
-    pass
-
-def pretty_task(value):
-    print_delim()
-    print("Task number", value)
-    pass
+import pretty
 
 # =========================================================================================================
 # Task 32.В программе phone_book реализовать следующие функции:
 # - find_entry_age_phonebook         # найти все записи с указанным возрастом
 # - print_phonebook_by_age           # распечатать все записи отсортированные по возрасту
-# - delete_entry_name_phonebook      # удалить все записи с указанным именем
-# - avr_age_of_all_persons           # распечатать средний возраст всех абонентов
+# - delete_entry_name_phonebook      # найти все записи с указанным именем
+# - print_avr_age                    # распечатать средний возраст всех абонентов
 # - increase_age                     # увеличить возраст всем абонентам на заданное пользователем кол-во лет
 # - <ваша_функция>                   # добавить любую функцию, манипулирующую записями (печать, добавление,
 #                                    удаление) в телефонной книге на ваше усмотрение.
@@ -32,7 +19,7 @@ def pretty_task(value):
 # именем нет, то вывести сообщение “Not found!”.
 # ==========================================================================================================
 
-pretty_task(32)
+pretty.pretty_task(32)
 
 
 
@@ -46,16 +33,8 @@ def print_entry(number, entry):
     print ("| Surname: %20s |" % entry["surname"])
     print ("| Name:    %20s |" % entry["name"])
     print ("| Age:     %20s |" % entry["age"])
-    print ("| Skype:   %20s |" % entry.get("Skype", "No Skype"))
-
-
     print ()
 
-def get_name(dict):
-    return dict["name"]
-
-def get_surname(dict):
-    return dict["surname"]
 
 def print_phonebook():
     print ()
@@ -63,59 +42,13 @@ def print_phonebook():
     print ("#########  Phone book  ##########")
     print ()
 
-    phone_book.sort(key=get_name)
-
     number = 1
-    for entry in phone_book:
-        print_entry(number, entry)
-        number += 1
-
-def print_phonebook_by_surname():
-    print ()
-    print ()
-    print ("#########  Phone book  ##########")
-    print ()
-
-    phone_book.sort(key=get_surname)
-
-    number = 1
-    for entry in phone_book:
-        print_entry(number, entry)
-        number += 1
-        found = True
-
-
-def get_age(dict):
-    return dict["age"]
-
-def get_skype(dict):
-    return dict["Skype"]
-
-def print_phonebook_by_skype():
-    print()
-    print()
-    print("#########  Phone book  ##########")
-    print()
-
-    number = 1
-    phone_book.sort(key=get_skype)
-
     for entry in phone_book:
         print_entry(number, entry)
         number += 1
 
 def print_phonebook_by_age():
-    print()
-    print()
-    print("#########  Phone book  ##########")
-    print()
-
-    number = 1
-    phone_book.sort(key=get_age)
-
-    for entry in phone_book:
-        print_entry(number, entry)
-        number += 1
+    pass
 
 def add_entry_phonebook(surname, name, age):
     entry = {}
@@ -141,14 +74,8 @@ def find_entry_name_phonebook(name):
     if not found:
         printError("Not found!!")
 
-def find_phonebook_by_skype(skype):
-    found = False
-    for entry in phone_book:
-        if entry.get("Skype", "No Skype") == skype:
-            print_entry(1, entry)
-            found = True
-    if not found:
-        printError("Not found!!")
+def avr_age_of_all_persons():
+    pass
 
 def find_entry_age_phonebook(age):
     idx = 1
@@ -161,40 +88,17 @@ def find_entry_age_phonebook(age):
     if not found:
         printError("Not found!!")
 
-
-def avr_age_of_all_persons():
-    sum_age = 0
-    for entry in phone_book:
-        sum_age += entry["age"]
-    avr_age = sum_age/len(phone_book)
-    print("Average age of person: %d" % avr_age)
-
 def delete_entry_name_phonebook(name):
-    found = False
-    for entry in phone_book:
-        if entry["name"] == name:
-            phone_book.remove(entry)
-            found = True
-    if not found:
-        printError("Not found!!")
+    pass
 
 def count_all_entries_in_phonebook():
     print ("Total number of entries: ", len(phone_book))
 
-def increase_age(number_of_years):
-    for i, entry in enumerate(phone_book):
-        phone_book[i]["age"] += number_of_years
-    print_phonebook_by_age()
+def print_avr_age():
+    pass
 
-def add_skype(name):
-    found = False
-    for i, entry in enumerate(phone_book):
-        if phone_book[i]["name"] == name:
-            skype = str(input("    Enter Skype: "))
-            phone_book[i]["Skype"] = skype
-            found = True
-    if not found:
-        printError("Not found!!")
+def increase_age(number_of_years):
+    pass
 
 def save_to_file():
     pickle.dump(phone_book, open("phone_book.save", "wb"))
@@ -225,9 +129,6 @@ def main():
             print ("     7 - The number of entries in the phonebook")
             print ("     8 - Avr. age of all persons")
             print ("     9 - Increase age by num. of years")
-            print ("     10 - Print phonebook entries (by surname)")
-            print ("     11 - Add Skype")
-            print ("     12 - Find phonebook entries (by Skype)")
             print ("-----------------------------")
             print ("     s - Save to file")
             print ("     l - Load from file")
@@ -261,19 +162,11 @@ def main():
             elif choice == 9:
                 nmbrs_of_years = int(input("    Enter number of years to add to current ages: "))
                 increase_age(nmbrs_of_years)
-            elif choice == 10:
-                print_phonebook_by_surname()
-            elif choice == 11:
-                name = str(input("    Enter name who add Skype: "))
-                add_skype(name)
-            elif choice == 12:
-                skype = str(input("    Enter skype: "))
-                find_phonebook_by_skype(skype)
             elif choice == 0:
                 print ("Bye!")
                 break
             else:
-                print ("Enter action within range [0..11]")
+                print ("Enter action within range [0..9]")
 
         except ValueError:
             if str(user_input) == 's':
