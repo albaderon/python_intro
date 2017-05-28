bf2639221bd3eaf6fd9b5ee8ede86a4d29251faf
albaderon <kravchukodessa@gmail.com>
2017-05-28 14:38:08 +0300

    Task 34

--- kravchuk_hw11.py
+++ kravchuk_hw11.py
@@ -17,31 +17,27 @@ class Godzilla:
     def __init__(self,eaten_human_volume=0,volume_of_stomach = 1000):
         self.eaten_human_volume = eaten_human_volume
         self.volume_of_stomach = volume_of_stomach
-        self.last_volume = volume_of_stomach
+        self.free_volume_space = volume_of_stomach
 
     def eat_humans(self,eat_human):
-        self.last_volume -= eat_human
+        self.free_volume_space -= eat_human
 
 
     def is_hungry(self):
-        if self.last_volume != Godzilla.PERSENT*godzilla_1.volume_of_stomach:
-            return True
-        else:
-            return False
-
+        return self.free_volume_space > Godzilla.PERSENT * godzilla_1.volume_of_stomach
 
 
 
 
 godzilla_1 = Godzilla()
 
-while godzilla_1.last_volume > Godzilla.PERSENT*godzilla_1.volume_of_stomach:
+while godzilla_1.is_hungry():
     godzilla_1.eaten_human_volume = random.randint(1,120)
-    if godzilla_1.last_volume - godzilla_1.eaten_human_volume < Godzilla.PERSENT*godzilla_1.volume_of_stomach:
+    if godzilla_1.free_volume_space - godzilla_1.eaten_human_volume < Godzilla.PERSENT*godzilla_1.volume_of_stomach:
         continue
     else:
         godzilla_1.eat_humans(godzilla_1.eaten_human_volume)
-        print("Human:",godzilla_1.eaten_human_volume,"nym nym nym","Last volume:", godzilla_1.last_volume)
+        print("Human:",godzilla_1.eaten_human_volume,"nym nym nym","Last volume:", godzilla_1.free_volume_space)
         if not godzilla_1.is_hungry():
             print("Godzilla is full and can no longer eat people")
 
